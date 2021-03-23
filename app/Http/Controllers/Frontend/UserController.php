165086ad40751c25dd3fd863e218d803ffb20a1e
Arup paul <aruppaulctg@gmail.com>
2021-03-23 12:04:26 +0600

    send sms

--- app/Http/Controllers/Frontend/UserController.php
+++ app/Http/Controllers/Frontend/UserController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Model\Cart;
+use App\Model\SMS;
 use App\User;
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
@@ -41,6 +43,22 @@ class UserController extends Controller
                         $session_id = Session::get('session_id');
                         Cart::where('session_id',$session_id)->update(['user_id' => $user_id]);
                     }
+
+//                    //send register message
+//                    $message = "Succesfully Register";
+//                    $mobile = $data['mobile'];
+//                    SMS::sendSms($message,$mobile);
+//
+//                    //send register email
+//                    $email = $data['email'];
+//                    $messageData = [
+//                        'name' => $data['name'],
+//                        'mobile' => $data['mobile'],
+//                        'email' => $data['email']
+//                    ];
+//                    Mail::send('emails.register',$messageData,function($message) use($email){
+//                        $message->to($email)->subject('Welcome to Ecommerce Website');
+//                    });
                    return redirect('cart');
                 }
             }
@@ -50,6 +68,7 @@ class UserController extends Controller
      public function login(Request $request){
         if($request->isMethod('post')){
             $data = $request->all();
+
             if(Auth::attempt(['email'  => $data['email'], 'password' => $data['password']])){
 
                 //update User cart with user id
@@ -58,6 +77,10 @@ class UserController extends Controller
                     $session_id = Session::get('session_id');
                     Cart::where('session_id',$session_id)->update(['user_id' => $user_id]);
                 }
+
+
+
+
                 return redirect('/cart');
             }else{
                 $message = 'Invalid Email or Password';
---
+++ resources/views/emails/register.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Register</title>
+</head>
+<body>
+<table>
+   <tr>
+       <td>Dear {{$name}}</td>
+   </tr>
+
+    <tr>
+        <td>Welcome to our website</td>
+    </tr>
+
+    <tr>
+        <td>&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td>Name: {{$name}}</td>
+    </tr>
+
+    <tr>
+        <td>&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td>Mobile: {{$mobile}}</td>
+    </tr>
+
+    <tr>
+        <td>&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td>Email: {{$email}}</td>
+    </tr>
+
+    <tr>
+        <td>&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td>Password ******* (as chosen by you)</td>
+    </tr>
+
+
+    <tr>
+        <td>Thanks & Regards</td>
+    </tr>
+
+    <tr>
+        <td>
+            Rio Ecommerce
+        </td>
+    </tr>
+
+
+
+</table>
+
+</body>
+</html>
